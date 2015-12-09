50449a94cc23ad0de528759bb57d2df8dad28f59
GunshipPenguin <167rhys@gmail.com>
2015-12-08 21:25:16 -0800

    Follow Minetest lua code style guidelines in arena.lua

--- mods/hungry_games/arena.lua
+++ mods/hungry_games/arena.lua
@@ -1,5 +1,6 @@
 arena = {}
 
+-- Register a node to fill the area outside the arena that should not be accessible to players
 minetest.register_node("hungry_games:arena_node", {
 	diggable = false,
 	sunlight_propagates = true,
@@ -7,6 +8,7 @@ minetest.register_node("hungry_games:arena_node", {
 	groups = {not_in_creative_inventory = 1}
 })
 
+-- Ensure that the only world generated is a cube centered around (0,0,0) with an x,y and z dimension of arena.size
 minetest.register_on_generated(function(minp, maxp, seed)
 	local c_arena = minetest.get_content_id("hungry_games:arena_node")
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
@@ -14,10 +16,10 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local data = vm:get_data(minp, maxp)
 	
 	for i in area:iterp(emin, emax) do
-		local currPos = area:position(i)
-		if math.abs(currPos["x"]) > arena.size/2 or 
-				math.abs(currPos["y"]) > arena.size/2 or
-				math.abs(currPos["z"]) > arena.size/2 then
+		local curr_pos = area:position(i)
+		if math.abs(curr_pos["x"]) > arena.size/2 or 
+				math.abs(curr_pos["y"]) > arena.size/2 or
+				math.abs(curr_pos["z"]) > arena.size/2 then
 			data[i] = c_arena;
 		end
 	end
